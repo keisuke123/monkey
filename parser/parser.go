8254cddba0ad731a11be2df49707df82c4dff165
Keisuke Kamiya <ozilikepop@gmail.com>
2018-10-25 23:47:40 +0900

    パーサの雛形追加

---
+++ parser/parser.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"github.com/keisuke123/monkey/ast"
+	"github.com/keisuke123/monkey/lexer"
+	"github.com/keisuke123/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
